8068317f24e24e4fbf8bfd90460e0c882d43b151
jbeggo <99847111+jbeggo@users.noreply.github.com>
2024-03-04 16:45:10 +0000

    cleanup useless

--- src/utils/random_AI.py
+++ src/utils/random_AI.py
@@ -1,11 +1,7 @@
 from utils.board import Board
-from copy import deepcopy
 import random
 
 def random_move(position: Board) -> tuple[int, int]:
-
-    bestMove = (20, 20)
-    bestEval = float('+inf')
 
     legal_moves = position.all_legal_moves(Board.WHITE)
 
