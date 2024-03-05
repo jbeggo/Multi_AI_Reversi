816ff6910dbf74141381d9b886b48dc2390a0679
jbeggo <99847111+jbeggo@users.noreply.github.com>
2024-03-05 05:14:31 +0000

    Bad syntax

--- src/utils/random_AI.py
+++ src/utils/random_AI.py
@@ -12,12 +12,12 @@ def random_move_older(position: Board) -> tuple[int, int]:
 def random_move_old(position: Board, player_colour: int) -> tuple[int, int]:
 
     # random player is white
-    if player_colour is -1:
+    if player_colour == -1:
 
         legal_moves = position.all_legal_moves(Board.WHITE)
 
     # random player is black
-    elif player_colour is 1:
+    elif player_colour == 1:
 
         legal_moves = position.all_legal_moves(Board.BLACK)
 
