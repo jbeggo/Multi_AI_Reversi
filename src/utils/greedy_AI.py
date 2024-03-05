758222bd7452fefab0c1b33f0fcc86f5467eadd7
jbeggo <99847111+jbeggo@users.noreply.github.com>
2024-03-05 05:08:59 +0000

    Simplified Minimax to extra greedy function

--- src/utils/greedy_AI.py
+++ src/utils/greedy_AI.py
@@ -28,3 +28,21 @@ def greedy(position: Board) -> tuple[int, int]:
                 final_move = (row,col)
             
     return final_move
+
+def greedy_move(position: Board, player_color: int) -> tuple[int, int]:
+    
+    # initialise
+    best_move = (20, 20)
+    best_eval = 0
+
+    legal_moves = position.all_legal_moves(player_color)
+    for row, col in legal_moves:
+        if position.board[row, col] == Board.EMPTY:
+            position_deepcopy = deepcopy(position)
+            position_deepcopy.set_discs(row, col, player_color)
+            eval = position_deepcopy.evaluate_board()
+            if eval >= best_eval:
+                best_move = (row, col)
+                best_eval = eval
+    
+    return best_move
